8c03eede5a36abbc89fac848385bc965b6cb936b
melinda rahmah <melinda.nda88@gmail.com>
2020-07-08 21:23:27 +0700

    membuat jumbotron gambar online

---
+++ resources/views/home.blade.php
@@ -0,0 +1,71 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <title></title>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+   <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.2/css/bootstrap.min.css">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.6/umd/popper.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.2/js/bootstrap.min.js"></script>
+  <style>
+   .carousel-inner img {
+      width: 100%;
+      height: 100%;
+  }
+  </style>
+</head>
+
+
+<body>
+<div class="container">
+  
+  	
+<div id="demo" class="carousel slide" data-ride="carousel">
+
+<!-- Indicators -->
+<ul class="carousel-indicators">
+  <li data-target="#demo" data-slide-to="0" class="active"></li>
+  <li data-target="#demo" data-slide-to="1"></li>
+  <li data-target="#demo" data-slide-to="2"></li>
+</ul>
+
+<!-- The slideshow -->
+<div class="carousel-inner">
+  <div class="carousel-item active">
+    <img src="https://www.umy.ac.id/wp-content/uploads/2020/06/Header-Masker-2020.jpeg" alt="Gambar - 1" width="1280" height="700">
+    <div class="carousel-caption">
+      <!-- <h3>Slide 1</h3>
+      <p>Deskripsi Slide 1</p> -->
+    </div>
+  </div>
+  <div class="carousel-item">
+    <img src="https://www.umy.ac.id/wp-content/uploads/2020/06/6AF2523C-1B1C-4AE4-85A7-16FD7E16E3FB-1350x450.png" alt="Gambar - 2" width="1280" height="700">
+    <div class="carousel-caption">
+      <!-- <h3>Slide 2</h3>
+      <p>Deskripsi Slide 2</p> -->
+    </div>
+  </div>
+  <div class="carousel-item">
+    <img src="https://www.umy.ac.id/wp-content/uploads/2020/03/WhatsApp-Image-2020-03-18-at-1.57.21-PM.jpeg" alt="Gambar - 3" width="1280" height="700">
+    <div class="carousel-caption">
+      <!-- <h3>Slide 3</h3>
+      <p>Deskripsi Slide 3</p> -->
+    </div>
+  </div>
+
+
+<!-- Left and right controls -->
+<a class="carousel-control-prev" href="#demo" data-slide="prev">
+  <span class="carousel-control-prev-icon"></span>
+</a>
+<a class="carousel-control-next" href="#demo" data-slide="next">
+  <span class="carousel-control-next-icon"></span>
+</a>
+</div>
+
+
+  </div>
+</div>
+</body>
+</html>
