b034d74d745442faa759e4fea14dea3a97d5d1c0
PanjiSatria <panjisatria1610@gmail.com>
2020-07-23 20:19:39 +0700

    fixed mahasiswa

--- app/Http/Controllers/MahasiswaController.php
+++ app/Http/Controllers/MahasiswaController.php
@@ -14,6 +14,8 @@ class MahasiswaController extends Controller
      */
     public function index()
     {
+        $mahasiswa = Mahasiswa::all();
+        return view ('mahasiswa', ['mahasiswa' => $mahasiswa]);
         //
     }
 
