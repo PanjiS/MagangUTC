3411e610c3d97d221f895a34eaebfcc9277e7e5b
PanjiSatria <panjisatria1610@gmail.com>
2020-08-07 09:49:31 +0700

    fixed pengelola contoller

--- app/Http/Controllers/PengelolaController.php
+++ app/Http/Controllers/PengelolaController.php
@@ -16,6 +16,8 @@ class PengelolaController extends Controller
       ->leftJoin('master_dosen','master_dosen.Lecture_Id','=','data_dosen_kelas.Lecture_Id')
       ->select('data_dosen_kelas.Department_Id', 'master_dosen.Lecture_Id', 'master_dosen.Lecture_Name', 'master_term_year.TermYear_Name', 'data_dosen_kelas.Course_Id', 'hasil_rekap_dosen.Status', 
         'hasil_rekap_dosen.Mean_IPK', 'hasil_rekap_dosen.nFailed', 'hasil_rekap_dosen.nBorderline', 'hasil_rekap_dosen.nStudents')
+      ->distinct('data_dosen_kelas.Department_Id', 'master_dosen.Lecture_Id', 'master_dosen.Lecture_Name', 'master_term_year.TermYear_Name', 'data_dosen_kelas.Course_Id', 'hasil_rekap_dosen.Status', 
+      'hasil_rekap_dosen.Mean_IPK', 'hasil_rekap_dosen.nFailed', 'hasil_rekap_dosen.nBorderline', 'hasil_rekap_dosen.nStudents')
       
       ->get();
       
@@ -29,6 +31,8 @@ class PengelolaController extends Controller
       ->leftJoin('master_term_year','master_term_year.TermYear_Id','=','data_nilai.TermYear_Id')
       ->select('data_nilai.Department_Id', 'data_nilai.Course_Id', 'master_term_year.TermYear_Name', 'hasil_rekap_matkul.Min', 'hasil_rekap_matkul.Max', 
         'hasil_rekap_matkul.Median', 'hasil_rekap_matkul.Mean', 'hasil_rekap_matkul.Standard_Deviation', 'hasil_rekap_matkul.Average_IPK')
+      ->distinct('data_nilai.Department_Id', 'data_nilai.Course_Id', 'master_term_year.TermYear_Name', 'hasil_rekap_matkul.Min', 'hasil_rekap_matkul.Max', 
+      'hasil_rekap_matkul.Median', 'hasil_rekap_matkul.Mean', 'hasil_rekap_matkul.Standard_Deviation', 'hasil_rekap_matkul.Average_IPK')
       
       ->get();
       
